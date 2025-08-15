2580125445a3a54b3156c726a32056170efa12e9
Ayushi Rathour <125720683+ayushirathour@users.noreply.github.com>
2025-08-15 15:36:42 +0530

    Update streamlit_app.py

--- api/streamlit_api_folder/streamlit_app.py
+++ api/streamlit_api_folder/streamlit_app.py
@@ -96,7 +96,9 @@ def load_pneumonia_model():
         except Exception:
             continue
     return None
-    if "pneumo_model" not in st.session_state:
+
+    
+if "pneumo_model" not in st.session_state:
         st.session_state["pneumo_model"] = load_pneumonia_model()
 
 
@@ -1040,6 +1042,7 @@ st.markdown(
 
 # Close container
 st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
